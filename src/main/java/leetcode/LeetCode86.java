6d26c2aebcaa42688454be27fa824320679b81c6
keboom <wangk@cogent-technologies.net>
2024-01-10 17:27:43 +0800

    feat: 贪心，回溯，二叉树

---
+++ src/main/java/leetcode/LeetCode86.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+import common.ListNode;
+
+/**
+ * {@code @author:} keboom
+ * {@code @date:} 2024/1/9
+ * {@code @description:}
+ */
+public class LeetCode86 {
+
+//    public ListNode partition(ListNode head, int x) {
+//
+//    }
+}
