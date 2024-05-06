8bc21c89b81f9a7f20025c2f5cd6999d6145b773
keboom <keboom777@gmail.com>
2024-05-06 16:21:57 +0800

    feat: 真是时光荏苒啊，21年做的题目，今天又从新做了一次。

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,25 +10,33 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
 
-
+        System.out.println(7 / 10);
 
     }
 
 
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        addNums(l1, l2, 0);
-    }
-
-    private ListNode addNums(ListNode l1, ListNode l2, int carry) {
-        if (l1 == null && l2 == null) {
-            if (carry > 0) {
-                ListNode addNode = new ListNode(carry);
-                return addNode;
+    /**
+     * 虚拟头节点。cur 是遍历到的当前节点。判断下一个和下下一个节点是不是重复，如果重复，删除。得到重复节点的 val，next 指针不停往下走
+     * 如果 val 等于重复节点的，则不管，直到 val 不等于重复节点。则 cur 指向 next
+     * @param head
+     * @return
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode cur = dummy;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int duplicateVal = cur.next.val;
+                ListNode next = cur.next.next;
+                while (next != null && next.val == duplicateVal) {
+                    next = next.next;
+                }
+                cur.next = next;
+            } else {
+                cur = cur.next;
             }
-        } else if (l1 == null && l2 != null) {
-            ListNode addNode = new ListNode((l2.val+carry)%10);
-            addNode.next = addNums()
-        }else if (l2)
+        }
+        return dummy.next;
     }
-
 }
--- src/main/java/leetcode/LeetCode82.java
+++ src/main/java/leetcode/LeetCode82.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import common.ListNode;
+
 /**
  * 删除排序链表中的重复元素||
  *
@@ -7,23 +9,6 @@ package leetcode;
  * @date 2021/7/9
  */
 public class LeetCode82 {
-
-    public class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
 
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null) {
@@ -38,6 +23,31 @@ public class LeetCode82 {
                 while (cur.next != null && cur.next.val == x) {
                     cur.next = cur.next.next;
                 }
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
+    }
+
+    /**
+     * 虚拟头节点。cur 是遍历到的当前节点。判断下一个和下下一个节点是不是重复，如果重复，删除。得到重复节点的 val，next 指针不停往下走
+     * 如果 val 等于重复节点的，则不管，直到 val 不等于重复节点。则 cur 指向 next
+     * @param head
+     * @return
+     */
+    public ListNode deleteDuplicates2(ListNode head) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode cur = dummy;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int duplicateVal = cur.next.val;
+                ListNode next = cur.next.next;
+                while (next != null && next.val == duplicateVal) {
+                    next = next.next;
+                }
+                cur.next = next;
             } else {
                 cur = cur.next;
             }
